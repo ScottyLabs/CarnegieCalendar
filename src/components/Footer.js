8ddb0ea2e7b968d311c4b388225cec0a9271d5cb
Gram Liu <gram.liu226@gmail.com>
2021-01-05 01:12:42 -0500

    Updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,20 @@ export default function HeaderRegion () {
     paddingTop: 37, 
     textAlign: "center",
   }
+
+  let attribCSS = {
+    fontSize: 11
+  }
   
   return (
     <Container text style={footerCSS}>
-      Developed by: ScottyLabs
+      Developed with <span role="img" aria-label="heart">❤️</span> by 
+      <a href="https://scottylabs.org" target="_blank"> ScottyLabs</a>
+      <br/>
+      <span style={attribCSS}>
+        Originally developed by
+        <a href="https://andykong.org" target="_blank"> Andy Kong</a>
+      </span>
     </Container>
   )
 }
